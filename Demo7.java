449f866f9fd145c2d3eab25c77f9860a1af07230
RAJA-501 <2311cs010501@mallareddyuniversity.ac.in>
2025-01-23 10:00:17 +0530

    Add files via upload

---
+++ Demo7.java
@@ -0,0 +1,13 @@
+
+
+public class Demo7 {
+    public static void main(String[] args) {
+        String Name = "raja";
+        int Age = 20;
+        long PhNO = 1234567890;
+        while ((Name == "raja") && (Age == 20) && (PhNO == 1234567890)) {
+            break;
+        }
+    }
+}
+
